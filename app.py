1ed934564e940085807cabbaf4cb93f0897f2431
Eric Wu <ericwu1995715@gmail.com>
2020-08-30 21:02:48 -0700

    viz

--- app.py
+++ app.py
@@ -2,15 +2,13 @@ import pandas as pd
 import streamlit as st 
 import numpy as np 
 import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy import stats
 import mpl_toolkits
 from datetime import datetime
-import seaborn as sn
 import os, time, sys
+import shap
+from pandas import DataFrame
 
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.datasets import make_regression
@@ -18,16 +16,24 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score, mean_squared_error
 from sklearn.metrics import accuracy_score
+from sklearn import tree
 
 st.title('Waka Waka Seattle Home Prices')
+st.write("""
+Our app predicts the **Seattle House Price**!
+""")
+st.write('---')
 
 from PIL import Image
 img = Image.open("seattle.jpg")
 st.image(img)
-
-st.write("""
-# Home Prices Predicted Using Machine Learning
+st.write('---')
+st.subheader("""
+Home Prices Predicted Using Machine Learning
 """)
+
+
+df = pd.read_csv('clean1.csv')
 
 
 #read and display csv
@@ -37,6 +43,22 @@ def fetch_data():
 
     return df
 
+
+y = df['price']
+X = df[['bedrooms',
+ 'bathrooms',
+ 'sqft_living',
+ 'sqft_lot',
+ 'floors',
+ 'waterfront',
+ 'view',
+ 'condition',
+ 'yr_built',
+ 'yr_renovated',
+ 'zipcode',
+ 'year',
+ 'month'
+]]
 
 #Select and split X and y
 def preprocessing(df):
@@ -60,14 +82,13 @@ def randomForest(X_train, X_test, y_train, y_test):
 def linearRegression(X_train, X_test, y_train, y_test):
     model = LinearRegression()
     model.fit(X_train,y_train)
-    y_predict = model.predict(X_test)
     score = model.score(X_test, y_test)*100
 
-    return score ,y_predict
+    return score , model
 
 
-#User input for the model
-def user_input():
+#User input for the Random Forest model
+def user_input_RF():
     bedrooms = st.slider("Bedrooms: ", 1,15)
     bathrooms = st.text_input("Bathrooms: ")
     sqft_living = st.text_input("Square Feet: ")
@@ -88,6 +109,28 @@ def user_input():
     return(user_input_prediction)
 
 
+#User input for linear regression model
+def user_input_LR():
+    bedrooms = st.slider("Bedrooms: ", 1,15)
+    bathrooms = st.number_input("Bathrooms: ",0.0,10.0,1.0,0.25)
+    sqft_living = st.number_input("Square Feet: ",0.0,30000.0,0.0,1.0)
+    sqft_lot = st.number_input("Lot Size: ",0.0,30000.0,0.0,1.0)
+    floors = st.number_input("Number Of Floors: ",0.0,5.0,1.0,0.25)
+    waterfront = st.number_input("Waterfront? For Yes type '1',  For No type '0': ",0.0,1.0,0.0,1.0)
+    view = st.slider("View (A higher score will mean a better view) : ", 0,4)
+    condition = st.slider("House Condition (A higher score will mean a better condition): ", 1,5)
+    yr_built = st.slider("Year Built: ",1900,2020)
+    yr_reno = st.slider("A Renovated Property? For Yes type '1',  For No type '0': ",0,1)
+    zipcode = st.number_input("Zipcode (5 digit): ",0.0,100000.0,0.0,1.0)
+    year_sold = st.number_input("Year Sold: ",0.0,100000.0,0.0,1.0)
+    month_sold = st.slider("Month Sold: ", 1,12)
+   
+    user_input_prediction = np.array([bedrooms,bathrooms,sqft_living,
+    sqft_lot,floors,waterfront,view,condition,yr_built,yr_reno,zipcode,year_sold,month_sold]).reshape(1,-1)
+
+    return(user_input_prediction)
+
+
 def main():
     data = fetch_data()
     X_train, X_test, y_train, y_test = preprocessing(data)
@@ -95,12 +138,30 @@ def main():
     if st.checkbox("Show the Data We Used"):
         st.subheader("Home Sales From 2014 to 2015")
         st.write(data.head(200))
+        if st.checkbox("Quick View of Features Histogram"):
+            data.hist(bins=50, figsize=(15,15))
+            # fig = ff.create_distplot(data, bins=50)
+            st.pyplot()
+    st.write('---')
 
     st.sidebar.header("Menu")
     st.sidebar.selectbox("Choose a City", ["Seattle"])
     ml_model = st.sidebar.selectbox("Choose a Model to Predict Home Prices", ["Random Forest Regressor","Mutilple Linear Regression","Coming Soon"])
+    viz = st.sidebar.selectbox("Visualization",['None','Feature Importance'])
+
 
     if(ml_model == "Random Forest Regressor"):
+        if(viz == 'Feature Importance'):
+            feature_names = X
+            score, regressor= randomForest(X_train, X_test, y_train, y_test)
+            importance = sorted(zip((regressor.feature_importances_), feature_names), reverse=True)
+            importance_df = DataFrame(importance, columns=['Feature_importances','Feature_names'])
+            importance_df.set_index('Feature_names',inplace=True)
+            st.markdown("Randon Forest feature importance")
+            st.bar_chart(importance_df)
+            st.write("---")
+        # st.pyplot()
+        st.subheader("Random Forest Model")
         score, regressor= randomForest(X_train, X_test, y_train, y_test)
         txt = "Accuracy of Random Forest Model is: " + str(round(score,2)) + "%" 
         if(st.checkbox("Show Model Accuracy")):
@@ -109,12 +170,11 @@ def main():
 
         try:
             if(st.checkbox("Start a Search")):
-                user_input_prediction = user_input()
+                user_input_prediction = user_input_RF()
                 pred = regressor.predict(user_input_prediction)
                 error = 94784
                 if(st.button("Submit")):
                     st.write('Mean Absolute Error: ', int(error))
-                    # st.write('The Predicted Home Price is: $', int(pred), u"\u00B1",int(error))
                     txt = 'The Predicted Home Price is: $' + str(int(pred)) + ' \u00B1 $' + str(error)
                     st.success(txt)
 
@@ -134,8 +194,9 @@ def main():
         except:
             pass
 
-    elif(ml_model == "Mutilple Linear Regression"):
-        score, y_predict= linearRegression(X_train, X_test, y_train, y_test)
+    if(ml_model == "Mutilple Linear Regression"):
+        st.subheader('Linear Regression Model')
+        score, model= linearRegression(X_train, X_test, y_train, y_test)
         txt = "Accuracy of Linear Regression Model is: " + str(round(score,2)) + "%. Proceed with caution" 
         if(st.checkbox("Show Model Accuracy")):
             st.warning(txt)
@@ -143,15 +204,27 @@ def main():
 
         try:
             if(st.checkbox("Start a Search")):
-                user_input_prediction = user_input()
+                user_input_prediction = user_input_LR()
                 pred = model.predict(user_input_prediction)
-                st.button("Submit")
-                    
-                    # txt = 'The Predicted Home Price is: $' + str(int(pred))
-                        # st.success(txt)
+                if(st.button("Submit")):
+                    txt = 'The Predicted Home Price is: $' + str(int(pred))
+                    st.success(txt)
 
+                feedback = st.radio("Waka Waka value your feedback, please rate from 1-5, (5) being excellent:", ('Please choose from below','1','2','3','4','5'))
+                if feedback == 'Please choose from below':
+                    st.text('')
+                elif feedback == '1': 
+                    st.error("This option is not valid")
+                elif feedback == '2': 
+                    st.warning("We have tried our best!")
+                elif feedback == '3': 
+                    st.success("Thank you for your feedback.")
+                elif feedback == '4': 
+                    st.success("Glad you like it!")
+                elif feedback == '5':
+                    st.success("Waka Waka agrees with you. Have a nice day!")
         except:
-            pass
+            st.write('error')
 
 
 
